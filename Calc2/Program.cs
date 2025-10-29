8cce3f22d0b43f9841452d10483494fb27927034
Андрей Полищук <dcnfandrey@mail.ru>
2025-10-29 20:19:02 +0300

    Multiplication & DivisionS DONE

--- Calc2/Program.cs
+++ Calc2/Program.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Schema;
 
 namespace Calc2
 {
@@ -13,7 +14,8 @@ namespace Calc2
 		static void Main(string[] args)
 		{
 			Console.Write("Введите арифметическое выражение: ");
-			string expression = "22+33-44/2+8*3";
+			string expression = "22*33/44/2*8*3";
+			//string expression = "22+33-44/2+8*3";
 			//string expression = Console.ReadLine();
 			expression = expression.Replace(".", ","); //Региональные настройки меняем точку на запятую
 			expression = expression.Replace(" ", ""); //Удаляем лишние пробелы (меняем пробел на пустую строку)
@@ -45,8 +47,24 @@ namespace Calc2
 			}
 			Console.WriteLine();
 
-
-
+			while (operations[0] != "")
+			{
+				int i = 0;
+				for (; i < operations.Length; i++)
+				{
+					if (operations[i] == "*" || operations[i] == "/")
+					{
+							if (operations[i] == "*") values[i] *= values[i + 1];
+							if (operations[i] == "/") values[i] /= values[i + 1];
+					}
+				for (int index = i; index < operations.Length-1; index++)	operations[index] = operations[index + 1];
+				for (int index = i+1; index < values.Length-1; index++) values[index] = values[index + 1];
+				operations[operations.Length - 1] = "";
+				values[values.Length - 1] = 0;
+					if (operations[i] == "*" || operations[i] == "/") i--;
+				}
+			}
+			Console.WriteLine(values[0]);
 
 #if CALC_IF
 			if (expression.Contains("+"))
@@ -78,7 +96,7 @@ namespace Calc2
 			} 
 #endif
 
-
 		}
 	}
 }
+
